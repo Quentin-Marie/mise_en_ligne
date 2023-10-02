e66e9d56ae4b7d870badcca58e57e4687ec8616b
Quentin-Marie <qmsoulis.dev@gmail.com>
2023-10-02 15:29:37 +0200

    redirection stripe

--- src/Controller/StripePayController.php
+++ src/Controller/StripePayController.php
@@ -22,7 +22,7 @@ class StripePayController extends AbstractController
         foreach ($fullCart as $item) {
             $line_items[] = [
                 'price_data' => [
-                    'unit_amount' => $item['activity']->getPrice()*10, 'currency' => 'EUR',
+                    'unit_amount' => $item['activity']->getPrice() * 10, 'currency' => 'EUR',
                     'product_data' => [
                         'name' => $item['activity']->getName()
                     ]
@@ -36,8 +36,10 @@ class StripePayController extends AbstractController
         \Stripe\Stripe::setApiKey('sk_test_51NwgsJIQPJJkuiXBepJX6I9xLi5YYo3JjY6VezvSyaQCX40N1LbF0bMrtp5izUM0o1NQ3KBrh88a8YRMHQLzpJMp00wJcuaLU0');
 
         $session = Session::create([
-            'success_url' => 'http://127.0.1.8000/commande/success',
-            'cancel_url' => 'http://127.0.1.8000/wishList',
+            // 'success_url' => 'http://127.0.1.8000/commande/success',
+            // 'cancel_url' => 'http://127.0.1.8000/wishList',
+            'success_url' => 'https://www.quentin.lock.cezdigit.com//commande/success',
+            'cancel_url' => 'https://www.quentin.lock.cezdigit.com//wishList',
             'payment_method_types' => ['card'],
             'line_items' => $line_items,
             'mode' => 'payment'
@@ -53,14 +55,14 @@ class StripePayController extends AbstractController
         if ($success) {
             $this->addFlash(
                 'success',
-                'Merci pour votre confiance');
+                'Merci pour votre confiance'
+            );
 
             return $this->redirectToRoute('app_front');
-
         } else {
             $this->addFlash(
-               'danger',
-               'Un problème est survenu, merci de recommencer votre paiement'
+                'danger',
+                'Un problème est survenu, merci de recommencer votre paiement'
             );
         }
 
